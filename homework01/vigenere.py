256fc2412c98526d0a701cf6065d19ab6605759b
polllkkka <144160004+polllkkka@users.noreply.github.com>
2023-10-31 23:44:55 +0300

    addign vigenre file

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,25 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    add1 = 0
+    while len(plaintext) > len(keyword):
+        keyword += keyword[add1]
+        add1 += 1
+    for i in range(len(keyword)):
+        if keyword[i].isupper():
+            key = ord(keyword[i]) - 65   # 65-й элемент - A. А - сдвиг на 0.
+        elif keyword[i].islower():
+            key = ord(keyword[i]) - 97  # 97-й элемент - а.
+        if plaintext[i].isalpha():
+            el1 = ord(plaintext[i])
+            if plaintext[i].isupper() and el1 >= 91 - key:
+                ciphertext += chr(el1 - 26 + key)
+            elif plaintext[i].islower() and el1 >= 123 - key:
+                ciphertext += chr(el1 - 26 + key)
+            else:
+                ciphertext += chr(el1 + key)
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
 
 
@@ -24,5 +42,23 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    add = 0
+    while len(ciphertext) > len(keyword):
+        keyword += keyword[add]
+        add += 1
+    for i, _ in enumerate(keyword):
+        if keyword[i].isupper():
+            key = ord(keyword[i]) - 65
+        elif keyword[i].islower():
+            key = ord(keyword[i]) - 97
+        if ciphertext[i].isalpha():
+            el = ord(ciphertext[i])
+            if ciphertext[i].isupper() and c_b <= 64 + key:
+                plaintext += chr(c_b + 26 - key)
+            elif ciphertext[i].islower() and c_b <= 96 + key:
+                plaintext += chr(c_b + 26 - key)
+            else:
+                plaintext += chr(c_b - key)
+        else:
+            plaintext += ciphertext[i]
     return plaintext
