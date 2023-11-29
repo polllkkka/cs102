b6bccccbca000e7299c6085aa1968ae27ed737c1
polllkkka <144160004+polllkkka@users.noreply.github.com>
2023-11-29 12:23:51 +0300

    реализована функция check_solution

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -188,8 +188,18 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
-
+    pos = find_empty_positions(grid)
+    if pos == (10, 10):
+        return grid
+    else:
+        n, m = find_empty_positions(grid)
+        res = find_possible_values(grid, pos)
+        for i in res:
+            grid[n][m] = str(i)
+            if solve(grid) is not None:
+                return grid
+            grid[n][m] = '.'
+        return None
 
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
     """ Если решение solution верно, то вернуть True, в противном случае False """
