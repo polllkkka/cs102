a32b9d4ff5814171e7c7f3d83b23b01138cebddf
polllkkka <144160004+polllkkka@users.noreply.github.com>
2024-03-12 22:36:52 +0300

    life proto black

--- homework04/life_proto.py
+++ homework04/life_proto.py
@@ -10,9 +10,7 @@ Grid = tp.List[Cells]
 
 
 class GameOfLife:
-    def __init__(
-        self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10
-    ) -> None:
+    def __init__(self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10) -> None:
         self.width = width
         self.height = height
         self.cell_size = cell_size
@@ -30,14 +28,14 @@ class GameOfLife:
         self.speed = speed
 
     def draw_lines(self) -> None:
-        """ Отрисовать сетку """
+        """Отрисовать сетку"""
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def run(self) -> None:
-        """ Запустить игру """
+        """Запустить игру"""
         pygame.init()
         clock = pygame.time.Clock()
         pygame.display.set_caption("Game of Life")
@@ -112,7 +110,6 @@ class GameOfLife:
         # Оставляет только те из них, которые заняты живыми клетками
         neighbours = set(filter(lambda c: c in cell, nearest_pairs))
         return neighbours
-
 
     def get_next_generation(self) -> Grid:
         """
