b496bffb3453e9ba0a86a95f31b06052a0fb6629
polllkkka <144160004+polllkkka@users.noreply.github.com>
2024-06-28 16:28:27 +0300

    all files added

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -2,7 +2,7 @@ from bottle import (
     route, run, template, request, redirect
 )
 
-from scrapper import get_news
+from scraputils import extract_news, get_news
 from db import News, session
 from bayes import NaiveBayesClassifier
 
@@ -16,19 +16,69 @@ def news_list():
 
 @route("/add_label/")
 def add_label():
-    # PUT YOUR CODE HERE
-    redirect("/news")
+    news_id = request.query.id
+    label = request.query.label
+    s = session()
+    news_item = s.query(News).filter(News.id == news_id).first()
+    if news_item:
+        news_item.label = label
+        s.commit()
+        s.close()
+        redirect("/news")
 
 
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
+    s = session()
+    news = get_news("https://news.ycombinator.com/newest", 5)
+    for news_item in news:
+        if not s.query(News).filter(News.title == news_item['title']).first():
+            new_news = News(title=news_item['title'], author=news_item['author'], url=news_item['url'])
+            s.add(new_news)
+    s.commit()
+    s.close()
     redirect("/news")
 
 
 @route("/classify")
 def classify_news():
-    # PUT YOUR CODE HERE
+    s = session()
+
+    labeled_news = s.query(News).filter(News.label is not None).all()
+    x_train = [news.title for news in labeled_news]
+    y_train = [news.label for news in labeled_news]
+
+    model = NaiveBayesClassifier(alpha=1.0)
+    model.fit(x_train, y_train)
+
+    unlabeled_news = s.query(News).filter(News.label == None).all()
+    x_test = [news.title for news in unlabeled_news]
+    predictions = model.predict(x_test)
+
+    for news, label in zip(unlabeled_news, predictions):
+        news.label = label
+    s.commit()
+    s.close()
+    redirect("/news")
+
+
+@route("/recommendations")
+def recommendations():
+    s = session()
+    _ = classify_news()
+    news = s.query(News).filter(News.label is not None).all()
+    first, second, third = [], [], []
+
+    for piece in news:
+        if piece.label == "good":
+            first.append(piece)
+        elif piece.label == "maybe":
+            second.append(piece)
+        elif piece.label == "never":
+            third.append(piece)
+    res = first + second + third
+
+    return template("news_recommendations", rows=res)
 
 
 if __name__ == "__main__":
